b8a1ef5593d7f70184d22e0adaa314f4f905b7cc
Daybreak <andrei1.popescu1@gmail.com>
2023-07-22 22:36:02 +0300

    good rendering

--- src/graphics/pieces.rs
+++ src/graphics/pieces.rs
@@ -1,4 +1,5 @@
 use std::ops::Deref;
+use std::pin::Pin;
 use bevy::prelude::*;
 use crate::board::components::Position;
 use crate::board::PIECE_Z;
@@ -14,11 +15,6 @@ pub fn spawn_piece_renderer(
 ) {
     for (entity, position, piece) in query.iter() {
         let v = super::get_world_position(&position, PIECE_Z);
-        let sprite_idx = match piece.kind.as_str() {
-            "Player" => 1,
-            _ => 63
-        };
-
         let sprite = assets.load("playerS.png");
         commands.entity(entity).insert(
             SpriteBundle{
@@ -34,19 +30,19 @@ pub fn spawn_piece_renderer(
 pub fn update_piece_position(
     mut commands: Commands,
     mut query: Query<(Entity, &Position, &mut Transform), With <Piece>>,
-    mut prop_query: Query<(Entity, &Transform, With<Prop>), Without<Piece>>,
+    mut prop_query: Query<(Entity, &Position, With<Prop>), Without<Piece>>,
     time: Res<Time>,
     assets: Res<AssetServer>,
 ) {
     for (entity, position, mut transform) in query.iter_mut() {
-        let mut target = super::get_world_position(&position, PIECE_Z);
-        for (prop_entity, prop_transform, prop) in prop_query.iter_mut() {
-            let prop_z = prop_transform.translation.z;
 
-            if target.z > prop_z {
-                target.z = target.z - 2.0;
-            }
+        let mut target = super::get_world_position(&position, PIECE_Z);
+
+        if prop_query.iter().any(|prop| prop.1.v.x < position.v.x) {
+            target.z -= 2.;
+
         }
+        println!("target.z {}", target.z);
 
         let d = (target - transform.translation).length();
 
