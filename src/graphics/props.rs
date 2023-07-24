2e07fc4e6897a05d78a9726ebd013684a8c3d830
Daybreak <andrei1.popescu1@gmail.com>
2023-07-24 18:32:16 +0300

    alpha channel

--- src/graphics/props.rs
+++ src/graphics/props.rs
@@ -8,6 +8,8 @@ use crate::pieces::components::Piece;
 use crate::player::Player;
 use crate::props::Prop;
 use crate::vectors::Vector2Int;
+use bevy::render::color::Color;
+use crate::graphics::get_world_position;
 
 pub fn update_prop_z(
     mut commands: Commands,
@@ -24,17 +26,25 @@ pub fn update_prop_z(
     if pos.is_none() {
         return;
     }
-    let pos = get_surrounding_coordinates(&pos.unwrap());
+    //let pos =
+    let pos = &pos.unwrap();
+    let scanned_positions = get_surrounding_coordinates(&pos);
 
     for (entity, position) in query.iter() {
 
         let sprite = assets.load("gard.png");
         let mut v = super::get_world_position(&position, PROP_Z);
+        let mut transparent_color = Color::rgba(1.0, 1.0, 1.0, 1.0);
 
-        if pos.contains(&position.v) {
+        if position.v.x < pos.x || position.v.y < pos.y {
             prop_z += 2.;
             v = super::get_world_position(&position, prop_z);
         }
+
+        if scanned_positions.contains(&position.v) {
+            transparent_color = Color::rgba(1.0, 1.0, 1.0, 0.5);
+        }
+
        println!("{}", prop_z);
 
 
@@ -42,6 +52,7 @@ pub fn update_prop_z(
             SpriteBundle {
                 texture: sprite,
                 transform: Transform::from_translation(v),
+                sprite: Sprite {color: transparent_color, ..default()},
                 ..Default::default()
             }
         ));
@@ -51,7 +62,7 @@ pub fn update_prop_z(
 fn get_surrounding_coordinates(v: &Vector2Int) -> Vec<Vector2Int> {
     let mut res = Vec::new();
 
-    let movements = vec![Vector2Int::new(-1, 0),Vector2Int::new(0, -1), Vector2Int::new(-1, -1), Vector2Int::new(-2, -1)];
+    let movements = vec![Vector2Int::new(-1, 0),Vector2Int::new(0, -1), Vector2Int::new(-1, -1)];
 
     for movement in &movements {
         let neighbor = *v + *movement;
